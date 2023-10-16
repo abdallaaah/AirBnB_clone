e031fde6bb84894904d2da3910d11f949480e769
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-15 17:41:13 -0700

    check saving new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,9 +22,9 @@ class FileStorge():
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, 'w', encoding='utf-8') as file_json:
-            json.dump(self.__objects, file_json, default=lambda obj: obj.to_dict())
-    
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(self.__objects, f, default=lambda obj: obj.to_dict())
+
     def reload(self):
         """ deserializes the JSON file to __objects"""
         if os.path.exists(self.__file_path):
@@ -33,6 +33,4 @@ class FileStorge():
                 try:
                     self.__objects = json.loads(data)
                 except json.JSONDecodeError:
-                    print("error from expected")
                     self.__objects = {}
-
